f563fdec36636dfb28af46e912ce2f3bb8e4a19e
Ferdea <sapegin04@mail.ru>
2022-11-04 18:03:48 +0300

    AutocompleteTask!

--- AutocompleteTask.cs
+++ AutocompleteTask.cs
@@ -31,7 +31,11 @@ namespace Autocomplete
         public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
         {
             // тут стоит использовать написанный ранее класс LeftBorderTask
-            return null;
+            return phrases
+                .Skip(LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1)
+                .Where(s => s.StartsWith(prefix))
+                .Take(count)
+                .ToArray();
         }
 
         /// <returns>
@@ -40,7 +44,8 @@ namespace Autocomplete
         public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
         {
             // тут стоит использовать написанные ранее классы LeftBorderTask и RightBorderTask
-            return -1;
+            return RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count) -
+                   LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) - 1;
         }
     }
 
@@ -50,19 +55,220 @@ namespace Autocomplete
         [Test]
         public void TopByPrefix_IsEmpty_WhenNoPhrases()
         {
-            // ...
-            //CollectionAssert.IsEmpty(actualTopWords);
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { }, new string[] { }, new string[] { }),
+                "aboba",
+                3
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
         }
-
-        // ...
 
         [Test]
-        public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+        public void TopByPrefix_AnyPhrases_WhenPrefixIsEmptyString()
         {
-            // ...
-            //Assert.AreEqual(expectedCount, actualCount);
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "aa", "ab", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                string.Empty,
+                3
+            );
+            CollectionAssert.AreEqual(new string[] { "aa a a", "ab a a", "ba a a" }, actualTopWords);
         }
 
-        // ...
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenPrefixLessThanAnyWord()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "aa",
+                3
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenPrefixMoreThanAnyWord()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "zz",
+                3
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenPrefixInPhrases()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "ac",
+                3
+            );
+            CollectionAssert.AreEqual(new string[] { "ac a a" }, actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenPrefixWordsInPhrases()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "ac", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                3
+            );
+            CollectionAssert.AreEqual(new string[] { "ac a a", "aca a a", "acb a a" }, actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenPrefixWordsNotInPhrases()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "ac", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ad",
+                3
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenPrefixNotInPhrases()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                3
+            );
+            CollectionAssert.AreEqual(new string[] { "aca a a", "acb a a" }, actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenCountPositiveButLessCountPrefixWords()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                1
+            );
+            CollectionAssert.AreEqual(new string[] { "aca a a" }, actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_WhenCountMoreCountPrefixWords()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                100
+            );
+            CollectionAssert.AreEqual(new string[] { "aca a a", "acb a a" }, actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenCountEqualZero()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                0
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenCountIsNegative()
+        {
+            var actualTopWords = AutocompleteTask.GetTopByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" },
+                    new string[] { "a" }),
+                "ac",
+                -5
+            );
+            CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        [Test]
+        public void СountByPrefix_IsZero_WhenNoPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { }, new string[] { }, new string[] { }),
+                "aboba"
+            );
+            Assert.AreEqual(0, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_WhenPrefixIsEmptyString()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "aa", "ab", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                string.Empty
+            );
+            Assert.AreEqual(5, actualCountWords);
+        }
+
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixLessThanAnyPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "aa"
+            );
+            Assert.AreEqual(0, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixMoreThanAnyPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "zz"
+            );
+            Assert.AreEqual(0, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixInPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "ac", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "ac"
+            );
+            Assert.AreEqual(1, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixWordsInPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "ac", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "ac"
+            );
+            Assert.AreEqual(3, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixWordsNotInPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "ac", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "ad"
+            );
+            Assert.AreEqual(0, actualCountWords);
+        }
+        
+        [Test]
+        public void СountByPrefix_IsZero_WhenPrefixNotInPhrases()
+        {
+            var actualCountWords = AutocompleteTask.GetCountByPrefix(
+                new Phrases(new string[] { "ab", "aca", "acb", "ba", "bc", "gg" }, new string[] { "a" }, new string[] { "a" }),
+                "ac"
+            );
+            Assert.AreEqual(2, actualCountWords);
+        }
     }
 }
